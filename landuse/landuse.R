1a6179ad6a381feac5358f1bdf58116f0a5b1619
ntabgoba <ntabgoba@gmail.com>
2016-11-10 20:53:15 +0900

    landuse extr

--- landuse/landuse.R
+++ landuse/landuse.R
@@ -5,9 +5,11 @@
 #(the datum or reference ellipsoid) for raw altitude data, and a gravitational equipotential surface 
 # (the geoid) that defines the nominal sea level.
 
-library(Rcmdr)
-attach(mtcars)
-scatter3d(wt, disp, mpg)
-install.packages("Rcmdr")
-library(rgl)
-install.packages("rgl")
+require(XML)
+landuse <- xmlParse(file = "L03-18M-07-01/KS-META-L03-18M_07.xml")
+
+landuse_data <- xmlToList(landuse)
+length(landuse_use)
+L03-18M-07-tky.dat
+
+readLines("L03-18M-07-01/L03-18M-07-tky.dat",n = 10)
