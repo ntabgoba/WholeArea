0050af8944f76d82303d23ebaecb8c6fa149b776
ntabgoba <ntabgoba@gmail.com>
2016-11-19 01:56:17 +0900

    create meshes frm lat/lon

--- whole44.R
+++ whole44.R
@@ -1,12 +1,16 @@
 #Whole area on a 4x4 km2
-air_11_15 <- read.csv("44/44whole.csv") #21350     8 dim, 105 NAME_2 level
+library(leaflet)
+library(dplyr)
+nukeicon <- makeIcon(iconUrl = "nukeicon.png",iconWidth = 18, iconHeight=18)
+
+air_11 <- read.csv("44/") #21350     8 dim, 105 NAME_2 level
 
 names(air_11_15) <- c("mdate","pref","city","NorthlatDec","EastlngDec",
                       "daichi_distance","AvgAirDoseRate","AvgAirDoseRateCm")
 air_11_15$mdate <- as.Date(air_11_15$mdate)
 air_11_15$pref <- as.character(air_11_15$pref)
 air_11_15$city <- as.character(air_11_15$city)
-
+air_11_15$gride <- latlong_to_meshcode(lat = air_11_15$NorthlatDec, long = air_11_15$EastlngDec,order = 3)
 #remove background radiations, jp govt sets at 0.04µSv/h
 air_11_15<- subset(air_11_15, AvgAirDoseRate > 0.04) #  21327     8
 #Calculate annual external dose rate
@@ -16,7 +20,7 @@ air_11_15$AnnualExtDose <- (air_11_15$AvgAirDoseRate - 0.04)*(8 + 16*0.4)*365/10
 air_11_15$AnnualExDoseRange <- cut(air_11_15$AnnualExtDose, c(0,1,3,5,10,20,50,100,200))
 #calculate area
 air_11_15AnnualExDoseRange_summary <- data.frame(table(air_11_15$AnnualExDoseRange))
-air_11_15AnnualExDoseRange_summary$Areakm2 <- 16 * air_11_15AnnualExDoseRange_summary$Freq
+air_11_15AnnualExDoseRange_summary$Areakm2 <- air_11_15AnnualExDoseRange_summary$Freq
 sum(air_11_15AnnualExDoseRange_summary$Areakm2)  # 341,200km²
 
 iro2 <- colorFactor(
@@ -34,5 +38,7 @@ air_11_15_plot <- leaflet() %>%
         addMarkers(lat = 37.4211, lng = 141.0328,icon = nukeicon)
 air_11_15_plot
 
+unique_gride <- unique()
 
+air_11_15_summary <- air_11_15 %>% summarise(group_by(unique(gride)) %>% transmute(MinADR = min(AnnualExtDose), MaxADR = max(AnnualExtDose),MeanADR = mean(AnnualExtDose), SdADR =sd(AnnualExtDose)))
 
